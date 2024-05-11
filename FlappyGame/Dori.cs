2ccaaed83c5aa282c4df618bc94b8b3d13b53151
Katemir <katyushka17122003@mail.ru>
2024-05-11 20:09:53 +0500

    Add files via upload

---
+++ FlappyGame/Dori.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyGame
+{
+    internal class Dori
+    {
+        public float x;
+        public float y;
+
+        public int sizeDori;
+      
+        public Image DoriImg;
+
+       
+
+        public Dori(int x, int y)
+        {
+            DoriImg = new Bitmap("C:\\Users\\User\\Desktop\\Новая папка\\прга учеба\\FlappyGame\\Arts\\necoarc.jpg");
+            this.x = x;
+            this.y = y;
+            sizeDori = 50;          
+            
+
+        }
+    }
+}
